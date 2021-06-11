a9fbcd2137223223268a7f599fd3afef3c0beab2
Tiago Mota <tiagodiggermota@hotmail.com>
2021-06-11 11:07:38 -0300

    Feat: Criação de programa Base-Salary

---
+++ Course/Entities/Worker.cs
@@ -0,0 +1,50 @@
+﻿using Course.Entities.Enums;
+using System.Collections.Generic;
+
+namespace Course.Entities
+{
+    class Worker
+    {
+        public string Name { get; set; }
+        public WorkerLevel Level { get; set; }
+        public double BaseSalary { get; set; }
+        public Department Department { get; set; } //associação entre duas classes diferentes
+        public List<HourContract> Contracts { get; private set; } = new List<HourContract>();//<HourContract> = vindo da classe HourContract / new List<HourContract>(); = instânciar para que não seja nula
+
+        public Worker()
+        {
+        }
+
+        public Worker(string name, WorkerLevel level, double baseSalary, Department department)
+        {
+            Name = name;
+            Level = level;
+            BaseSalary = baseSalary;
+            Department = department;
+            // contract não está aqui, pois não se passa lista instânciada (assossiada para muitos) no construtor
+        }
+
+        public void AddContract(HourContract contract) //operação AddContract = para adicionar contrato ao trabalhador
+        {
+            Contracts.Add(contract);
+        }
+
+        public void RemoveContract(HourContract contract)  //operação RemoveContract = para remover contrato desse trabalhador
+        {
+            Contracts.Remove(contract);
+        }
+
+        public double Income(int year, int month) // operação para retornar quanto o funcionário ganhou em um determinado ano e mês
+        {
+            double sum = BaseSalary; //BaseSalary ao invez de 0, pois o trabalhador receberá um salário independentemente de algum contrato
+            foreach (HourContract contract in Contracts)// para percorrer a lista de contratos
+            {
+                if (contract.Date.Year == year && contract.Date.Month == month)// se o ano e mês do contrato for o mesmo que recebi do argumento, então entra na soma
+                {
+                    sum += contract.TotalValue(); //soma recebe ela mesma (que é = ao salário) mais valor do contrato
+                }
+            }
+            return sum;
+        }
+    }
+}
---
+++ Course/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Globalization;
+using Course.Entities;
+using Course.Entities.Enums;
+
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Enter department's name: ");
+            string deptName = Console.ReadLine();
+            Console.WriteLine("Enter worker data:");
+            Console.Write("Name: ");
+            string name = Console.ReadLine();
+            Console.Write("Level: (Junior/MidLevel/Senior): ");
+            WorkerLevel level = Enum.Parse<WorkerLevel>(Console.ReadLine()); //receber o string de função do funcionério e converter para workerLevel
+            Console.Write("Base salary: ");
+            double baseSalary = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Department dept = new Department(deptName); //instanciando o objeto departamento
+            Worker worker = new Worker(name, level, baseSalary, dept);//instanciando o objeto worker
+
+            Console.Write("How many contracts to this worker? ");
+            int n = int.Parse(Console.ReadLine()); //GUARDAR NA VARIÁVEL N O QUE O USUÁRIO DIGITAR (READLINE)
+
+            for (int i = 1; i <= n; i++)
+            {
+                Console.WriteLine($"Enter #{i} contract data:"); //$ #{i} para interpolação
+                Console.Write("Date (DD/MM/YYYY): ");
+                DateTime date = DateTime.Parse(Console.ReadLine());// datatime.parse para converter padrão americano para brasileiro
+                Console.Write("Value per hour: ");
+                double valuePerHour = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                Console.Write("Duration (hours): ");
+                int hours = int.Parse(Console.ReadLine());
+                HourContract contract = new HourContract(date, valuePerHour, hours);
+                worker.AddContract(contract);// PARA ADICIONAR O CONTRATO QUE ACABEI DE INSTANCIAR
+            }
+
+            Console.WriteLine();
+            Console.Write("Enter month and year to calculate income (MM/YYYY): ");
+            string monthAndYear = Console.ReadLine();
+            int month = int.Parse(monthAndYear.Substring(0, 2));//GUARDAR UM RECORTE DO MÊS DIGITADO NA VARIÁVEL MONTH A PARTIR DO ZERO ATÉ O SEGUNDO CARACTER
+            int year = int.Parse(monthAndYear.Substring(3));//GUARDAR UM RECORTE DO ANO DIGITADO NA VARIÁVEL YEAR A PARTIR DO TERCEIRO CARACTER
+            Console.WriteLine("Name : " + worker.Name);
+            Console.WriteLine("Department: " + worker.Department.Name);
+            Console.WriteLine("Income for " + monthAndYear + ": " + worker.Income(year, month).ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
